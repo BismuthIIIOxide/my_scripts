ebc2a140a93679eac382c04779255b268ab5966c
Bismuth(III) Oxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2020-12-09 11:12:51 -0500

    im retarded

--- wiki.lua
+++ wiki.lua
@@ -1,11 +1,7 @@
--- test thing im doing cause im bored
--- inspired from:
---              https://github.com/goldsmith/Wikipedia
-
-wiki = {}
+local wiki = {}
 
 local HS = game:GetService("HttpService")
-local function wiki:random()
+function wiki:random()
     local URL = "https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json"
     local req = HS:JSONDecode(game:HttpGet(URL))
 
@@ -20,15 +16,18 @@ end
 function wiki:summary(Q, N) 
     N = N or 1 
     local sent = {}
-    local info = search(Q)
+    local info = wiki:search(Q)
     local title = info['title']
-    title = title:gsub('%s+','+') -- makes it so "title" is easier to search up
-    -- "Barack Obama" -> "Barack+Obama"
+    title = title:gsub('%s+','+') -- makes it so "title" is easier to search up, not sure if i even need to do this tbh
+                                  -- "Barack Obama" -> "Barack+Obama"
     local ID = info['pageid']
     ID = tostring(ID)
     local S = 'https://en.wikipedia.org/w/api.php?prop=extracts&explaintext=&titles='..title..'&exsentences='..N..'&action=query&format=json'
     local req = HS:JSONDecode(game:HttpGet(S))
-    req = req['query']['pages'][ID]['extract']; req = req:gsub('%b()',''); req = req:gsub('%s%s',' ') -- removes anything in parentheses
+    req = req['query']['pages'][ID]['extract']; 
+    req = req:gsub('%b()',''); req = req:gsub('%s%s',' ') -- removes anything in parentheses 
+                                                          -- this is personal preference and can easily be removed/commented out
     
     return req
 end
+getgenv()["wiki"] = wiki
