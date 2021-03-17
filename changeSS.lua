d01b63909417a2bf6f909c846065b057397027a3
Bismuth(III) Oxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2021-03-17 14:35:28 -0400

    Update changeSS.lua

--- changeSS.lua
+++ changeSS.lua
@@ -1,6 +1,4 @@
 local change = {}
-local lp = game.Players.LocalPlayer
-local pstats = lp.pstats
 
 function change:Other(STAT, VAL)
     STAT.Value = VAL
@@ -8,45 +6,45 @@ end
 
 -- not stats
 function change:Style(VAL)
-    pstats.style.Value = VAL
-    pstats.stylestate.Value = 4
+    game.Players.LocalPlayer.pstats.style.Value = VAL
+    game.Players.LocalPlayer.pstats.stylestate.Value = 4
 end
 function change:Element(VAL)
-    pstats.element.Value = VAL
+    game.Players.LocalPlayer.pstats.element.Value = VAL
 end
 function change:Clan(VAL)
-    pstats.clan.Value = VAL
+    game.Players.LocalPlayer.pstats.clan.Value = VAL
 end
 function change:Armor(VAL)
-    lp.soularmor.Value = VAL
+    game.Players.LocalPlayer.soularmor.Value = VAL
 end
 
 -- stats
 function change:Strength(VAL)
-    pstats.strength.Value = VAL
+    game.Players.LocalPlayer.pstats.strength.Value = VAL
 end
 function change:Endurance(VAL)
-    pstats.endurance.Value = VAL
+    game.Players.LocalPlayer.pstats.endurance.Value = VAL
 end
 function change:Agility(VAL)
-    pstats.agility.Value = VAL
+    game.Players.LocalPlayer.pstats.agility.Value = VAL
 end
 function change:Speed(VAL)
-    pstats.speed.Value = VAL
+    game.Players.LocalPlayer.pstats.speed.Value = VAL
 end
 function change:Mana(VAL)
-    pstats.mana.Value = VAL
+    game.Players.LocalPlayer.pstats.mana.Value = VAL
 end
 function change:Combatexp(VAL)
-    pstats.combatexp.Value = VAL
+    game.Players.LocalPlayer.pstats.combatexp.Value = VAL
 end
 
 -- for fun
 function change:Phrase(VAL)
-    lp.phrase.Value = VAL
+    game.Players.LocalPlayer.phrase.Value = VAL
 end
 function change:Name(VAL)
-    lp.guardianname.Value = VAL
+    game.Players.LocalPlayer.guardianname.Value = VAL
 end
 
 
